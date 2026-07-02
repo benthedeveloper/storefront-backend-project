caf689af0819a9d707aed3b4b53faf58f0e1b852
Ben Harrison <benthedeveloper@users.noreply.github.com>
2026-07-02 13:07:33 -0700

    refactor: Change toCamelCase function to arrow function

--- src/helpers/caseConverters.ts
+++ src/helpers/caseConverters.ts
@@ -1,8 +1,12 @@
-export function toCamelCase(obj: Record<string, any>): Record<string, any> {
-  const camelCased: Record<string, any> = {};
+export const toCamelCase = (obj: Record<string, unknown>): Record<string, unknown> => {
+  const camelCased: Record<string, unknown> = {};
+
   for (const key in obj) {
-    const camelKey = key.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
-    camelCased[camelKey] = obj[key];
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      const camelKey = key.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
+      camelCased[camelKey] = obj[key];
+    }
   }
+
   return camelCased;
-}
+};
